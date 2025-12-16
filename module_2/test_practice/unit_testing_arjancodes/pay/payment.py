34febc62df89567ae05cb7259c1cfb7bfca9c378
Hasna Hena Mow <hasna.hena@welldev.io>
2025-12-16 16:18:33 +0600

    refactored code with dependency injection and mocking class

--- module_2/test_practice/unit_testing_arjancodes/pay/payment.py
+++ module_2/test_practice/unit_testing_arjancodes/pay/payment.py
@@ -1,13 +1,18 @@
+from typing import Protocol
+
 from pay.order import Order
 from pay.processor import PaymentProcessor
 
+# class PaymentProcessor(Protocol):
+#     def charge(self, card: str, month: int, year: int, amount: int) -> None:
+#         """Charges the card with given amount"""
 
-def pay_order(order: Order):
+def pay_order(order: Order, processor: PaymentProcessor) -> None:
     if order.total == 0:
         raise ValueError("Can't pay an order with total 0.")
     card = input("Please enter your card number: ")
     month = int(input("Please enter the card expiry month: "))
     year = int(input("Please enter the card expiry year: "))
-    payment_processor = PaymentProcessor("6cfb67f3-6281-4031-b893-ea85db0dce20")
-    payment_processor.charge(card, month, year, amount=order.total)
+    # payment_processor = PaymentProcessor("6cfb67f3-6281-4031-b893-ea85db0dce20")
+    processor.charge(card, month, year, amount=order.total)
     order.pay()
--- module_2/test_practice/unit_testing_arjancodes/pay/tests/test_payment.py
+++ module_2/test_practice/unit_testing_arjancodes/pay/tests/test_payment.py
@@ -4,28 +4,25 @@ from pay.order import LineItem, Order
 from pay.payment import pay_order
 from pytest import MonkeyPatch
 
-from pay.processor import PaymentProcessor
 
+class PaymentProcessorMock:
+    def charge(self, card: str, month: int, year: int, amount: int) -> None:
+        print(f"Charged {amount} on {card}")
 
 def test_pay_order(monkeypatch: MonkeyPatch) -> None:
-    def charge_mock(self, card: str, month: int, year: int, amount: int) -> None:
-        pass
+
     inputs = ["1249190007575069", 12, 2026]
 
     monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    monkeypatch.setattr(PaymentProcessor, "_check_api_key", lambda _: True)
-    monkeypatch.setattr(PaymentProcessor, "charge", charge_mock)
-
     order = Order()
     order.line_items.append(LineItem("Test", 200))
-    pay_order(order)
+    pay_order(order, PaymentProcessorMock())
 
 def test_pay_order_invalid(monkeypatch: MonkeyPatch) -> None:
     with pytest.raises(ValueError):
         inputs = ["1249190007575069", 12, 2026]
 
         monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-        monkeypatch.setattr(PaymentProcessor, "_check_api_key", lambda _: True)
 
         order = Order()
-        pay_order(order)
+        pay_order(order, PaymentProcessorMock())
