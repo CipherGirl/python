80a36a05719d6ad1a18f6a466ca5cb25c7710f6f
Hasna Hena Mow <hasna.hena@welldev.io>
2025-12-18 16:04:23 +0600

    numpy: slicing example

--- module_2/numpy/main.py
+++ module_2/numpy/main.py
@@ -30,3 +30,26 @@ print(multi_dim_array[0, 1]) # Multi dimensional indexing (Numpy)
 ace = multi_dim_array[0, 0] + multi_dim_array[0, 2] + multi_dim_array[1, 1]
 
 print(ace)
+
+
+#========== SLICING ==========
+
+array_2d = np.array([[1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                    [9, 10, 11, 12],
+                    [13, 14, 15, 16]])
+
+# array[start:end:step]
+# print(array_2d[::-2])
+
+# Col/Row Selection
+# print(array_2d[:, 1:4])
+
+print(array_2d[0:2, 0:2])
+print(array_2d[0:2, 2:])
+print(array_2d[2:, 0:2])
+print(array_2d[2:, 2:])
+
+
+
+
