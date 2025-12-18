60fef24fb530071ff37c110c74c2721b31cae524
Hasna Hena Mow <hasna.hena@welldev.io>
2025-12-18 15:07:08 +0600

    pandas: loading dataframe

---
+++ module_2/pandas/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Dataframe - 2d Labeled data structure (like spreadsheet or sql)
+#   Key Features - Label based indexing, Col-Row Wise operations, Supports mixed data type, fast and vectorized operations
+
+# Series - 1d labeled array (like column)
+
+# pd.read_csv, pd.read_excel, pr.dataFrame(dictionary)
+df = pd.read_csv("/Users/welldev/Code/self-development/python/module_2/pandas/data.csv")
+    # pd.
+print(df)
+
+ 
+
