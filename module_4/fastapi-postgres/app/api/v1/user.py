681470c087e33e76a58c3ecc2272b47bc75878e9
Hasna Hena Mow <hasna.hena@welldev.io>
2026-01-27 15:47:42 +0600

    api: get all users and user by id route and service

--- module_4/fastapi-postgres/app/api/v1/user.py
+++ module_4/fastapi-postgres/app/api/v1/user.py
@@ -4,6 +4,7 @@ from psycopg2.extensions import connection
 from app.schemas.user import UserCreate, UserResponse
 from app.services.user import UserService
 from app.core.database import db_generator
+from typing import List
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -26,4 +27,23 @@ def create_user(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Error creating user: {str(e)}"
         )
+
+@router.get("/", response_model=List[UserResponse])
+def get_users(db: connection = Depends(db_generator)):
+    users = UserService.get_all_users(db)
+    return users
+
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(
+    user_id: int, 
+    db: connection = Depends(db_generator)
+):
+    user = UserService.get_user_by_id(db, user_id)
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    return user
 
