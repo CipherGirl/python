abc9a16549886a11dff077bc91d9d783d20569e9
Hasna Hena Mow <hasna.hena@welldev.io>
2026-01-27 14:55:22 +0600

    schema: pydantic schema for user

---
+++ module_4/fastapi-postgres/app/schemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+class UserCreate(UserBase):
+    pass
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    full_name: Optional[str] = None
+    is_active: Optional[bool] = None
+
+class UserResponse(UserBase):
+    id: int
+    is_active: bool
+    created_at: datetime
